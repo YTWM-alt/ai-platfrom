7d7b39b4026d9584ef6437813f3f6cc6da456cba
xkins <xk26_70wx@163.com>
2026-02-02 00:04:33 +0800

    位置挪动与图标更改

--- components/private/siderbar/Sidebar.tsx
+++ components/private/siderbar/Sidebar.tsx
@@ -4,9 +4,9 @@ import { usePathname, useRouter } from 'next/navigation';
 import { SearchOutlined, LeftOutlined, RightOutlined, HomeOutlined  } from '@ant-design/icons';
 import { NAV_ITEMS } from './SiderbarData'; // 导入配置
 import { SidebarItem } from './SidebarItem'; // 导入零件
-import LoginModal from '../User/LoginModal';
+import LoginModal from '../../User/LoginModal';
 import { useLayout } from '@/context/LayoutContext';
-import { UserSection } from '../User/UserSection';
+import { UserSection } from '../../User/UserSection';
 
 // 增加 props 接口，方便父组件控制或同步状态
 interface SidebarProps {
@@ -47,13 +47,13 @@ return (
         {/* === 顶部 Logo 区域 === */}
         <div className="shrink-0 px-4 pt-6 pb-2 border-b border-gray-200 flex flex-col gap-2">
           <div className="flex items-center gap-2 px-1 mb-1">
-            <div className="w-8 h-8 bg-primary flex items-center justify-center text-white shadow-sm shrink-0">
-              <div className="w-8 h-8 relative shrink-0">
+            <div className="w-9 h-9 flex items-center justify-center shrink-0">
+              <div className="w-9 h-9 relative shrink-0">
                 <Image 
                   src="/favicon.svg"   
                   alt="Logo"
-                  fill                 // 填充父容器
-                  priority             // 侧边栏 Logo 通常是首屏关键元素，建议加 priority 加快加载速度
+                  fill              // 填充父容器   
+                  priority          // 侧边栏 Logo 通常是首屏关键元素，建议加 priority 加快加载速度   
                   className="object-contain" 
                 />
               </div>
